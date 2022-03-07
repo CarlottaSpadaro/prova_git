a8d09ebfeb8f60bbaff9291f824e901cf8ab1c26
spada <spada@172.22.104.60>
2022-03-07 09:15:28 +0100

    Versione iniziale

---
+++ src/main/java/it/polito/tdp/prova_git/Main.java
@@ -0,0 +1,9 @@
+package it.polito.tdp.prova_git;
+
+public class Main {
+	public static void main(String[] args) {
+        EntryPoint.main(args);
+    }
+}
+
+//tasto destro su progetto -> team -> "share project" -> spunta la prima casella -> clicca nella prima riga -> tasto in basso a sx -> "create repository"
